38523a859ded72a6261e1df1a99eeaf84956e5c9
EC2 Default User <ec2-user@ip-172-31-13-127.us-west-1.compute.internal>
2019-08-06 02:31:10 +0000

    change db connect

--- db/postgres.js
+++ db/postgres.js
@@ -1,13 +1,10 @@
 const {Client} = require('pg');
 
 const client = new Client({
-  user: 'davidbrooks',
-<<<<<<< HEAD
-  host: 'http://54.183.41.105/5432',
-=======
-  host: 'http://54.183.41.105:5432',
->>>>>>> a41822a2926974007f1e6691dceb1de3d6851d60
-  database: 'resturants',
+  user: 'power_user',
+  host: 'ec2-54-183-41-105.us-west-1.compute.amazonaws.com',
+  database: 'restaurants',
+  password: 'root'
 });
 client.connect();
 
