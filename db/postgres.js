8de8a66963bbfd15b28abc2fbdf1b1cec0408e5a
EC2 Default User <ec2-user@ip-172-31-13-127.us-west-1.compute.internal>
2019-08-06 01:25:51 +0000

    relic

--- db/postgres.js
+++ db/postgres.js
@@ -2,7 +2,7 @@ const {Client} = require('pg');
 
 const client = new Client({
   user: 'davidbrooks',
-  host: 'http://54.183.221.234.5432',
+  host: 'http://54.183.41.105/5432',
   database: 'resturants',
 });
 client.connect();
