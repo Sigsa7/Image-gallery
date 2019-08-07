9a4db2abc860207f7c7db2d5fb85d46125f05fe8
David Brooks <davidbrooks@Davids-MacBook-Pro-2.local>
2019-08-06 19:34:19 -0700

    Add pool

--- db/postgres.js
+++ db/postgres.js
@@ -1,5 +1,5 @@
-const {Client} = require('pg');
-const client = new Client({
+const {Pool} = require('pg');
+const client = new Pool({
   user: 'power_user',
   host: 'ec2-54-219-137-225.us-west-1.compute.amazonaws.com',
   password: 'root',
